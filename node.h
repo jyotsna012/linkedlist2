a894cd690f16ce98ba629f4b5e71935535863e08
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-30 13:03:13 -0800

    Update node.h

--- node.h
+++ node.h
@@ -4,12 +4,18 @@
 class Node{
 
  public:
+  //constructor
   Node(Student* newS);
+ //get next
   Node* getNext();
   Student* getStudent();
+ // set next
   void setNext(Node* nNext);
+ //destructor
   ~Node();
  private:
+ // variable for the node's student
   Student* student;
+ //variable for next 
   Node* next;
 };
